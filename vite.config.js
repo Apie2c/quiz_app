7c5dd49eafa158a0c866fa20b64bff18304b6265
Daniel <apie2c@gmail.com>
2025-12-25 23:40:06 +0000

    new

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-// ============================================
-// FILE 10: vite.config.js
-// ============================================
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: 'public', // Tells Vite the HTML is here
+  build: {
+    outDir: '../dist', // Tells Vite to put the result back in the main dist folder
+  },
   server: {
     proxy: {
       '/api': {
